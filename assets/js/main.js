e56b0139b42ca65d13ce6acd99f61ea4a11b5850
Mohammad Mostafavi <mr.mohammadmostafavi@gmail.com>
2023-10-12 23:20:58 +0330

    Complete Navbar Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
 // project scripts
+
+function myFunc() {
+    var nav = document.getElementById("nav");
+    if (nav.className === "nav") {
+        nav.className += " responsive";
+    } else {
+        nav.className = "nav";
+    }
+}
