0890420c273d8b28fe278ffbaa13207d4f2bed25
Mohammad Mostafavi <mr.mohammadmostafavi@gmail.com>
2023-10-13 14:51:55 +0330

    fix-bug-in-navbar-responsive-view

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,9 @@ function myFunc() {
     var nav = document.getElementById("nav");
     if (nav.className === "nav") {
         nav.className += " responsive";
+        console.log(nav.className);
     } else {
         nav.className = "nav";
+        console.log(nav.className);
     }
 }
